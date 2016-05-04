a908f7e3c302fa6e6821b46099cd06f0ed4eb27c
P.R <kuripa1lp@gmail.com>
2016-05-04 09:20:42 +0200

    spielsteinSetzen begonnen

--- VIER_GEWINNT/SPIELFELD.java
+++ VIER_GEWINNT/SPIELFELD.java
@@ -10,12 +10,23 @@ public class SPIELFELD
     int breite;
     int hoehe;
     int spielfeld[][];
-    
+
     public SPIELFELD()
     {
-    
+
     }
 
-    
-    
+    /**
+     * setzt Spielstein in Spalte x
+     * ganz links 0
+     * ganz rechts breite-1
+     * 
+     * @param x Spalte 
+     * @param spieler 1: Spieler1; 2: Spieler2
+     * @return setzen ausgefuehrt?
+     */
+    public boolean spielSteinSetzen (int x, int spieler)
+    {
+        return false;
+    }
 }
