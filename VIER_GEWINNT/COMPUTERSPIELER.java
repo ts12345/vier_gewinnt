661945422ac60fb761aa8c7ab4c847ba9aebaf36
allgemein <allgemein>
2016-05-13 09:48:00 +0200

    jetzt aber

---
+++ VIER_GEWINNT/COMPUTERSPIELER.java
@@ -0,0 +1,21 @@
+
+/**
+ * Beschreiben Sie hier die Klasse COMPUTERSPIELER.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class COMPUTERSPIELER
+{
+    /**
+     * Konstruktor für Objekte der Klasse COMPUTERSPIELER
+     */
+    public COMPUTERSPIELER()
+    {
+
+    }
+
+    public int getNextMove(){
+        return 1;
+    }
+}
