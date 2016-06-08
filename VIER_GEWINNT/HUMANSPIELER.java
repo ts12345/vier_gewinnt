c2a5d9dec318ec84ced1296a3810a454d9c7d839
allgemein <allgemein>
2016-06-08 08:42:40 +0200

    Schlafen beim Warten auf Klick

--- VIER_GEWINNT/HUMANSPIELER.java
+++ VIER_GEWINNT/HUMANSPIELER.java
@@ -7,23 +7,30 @@ import java.awt.event.*;
  */
 public class HUMANSPIELER extends SPIELER
 {
-    
+
     /**
      * Konstruktor für Objekte der Klasse HUMANSPIELER
      */
     public HUMANSPIELER()
     {
     }
+
     public int getNextMove()
     {
+        System.out.println("am zug");     
         LISTENER.startListening();
-        
+
         while(LISTENER.getLastX() == 0)
         {
+            System.out.println(LISTENER.getLastX());
+
+            try {
+                Thread.sleep(100);  // milliseconds
+            } catch (InterruptedException ex) { } 
             //Warten darauf dass geklickt wird
             //evtl. wait(100)
         }
-        
+
         System.out.println("Next Move : " + (LISTENER.getLastX()/100));
         return (int)(LISTENER.getLastX()/100);
     }
--- VIER_GEWINNT/LISTENER.java
+++ VIER_GEWINNT/LISTENER.java
@@ -7,41 +7,47 @@ public class LISTENER implements MouseListener
     private CONTROLLER controller;
     static int lastX;
     static int lastY;
-    
+
     public LISTENER(CONTROLLER controller)
     {
         this.controller = controller;
     }
-    
-    
+
     public void mouseExited(MouseEvent e)
     {
     }
+
     public void mouseMoved(MouseEvent e)
     {
     }
+
     public void mouseClicked(MouseEvent e)
     {
-        //System.out.println("Click");
+
         lastX = e.getX();
         lastY = e.getY();
+        System.out.println("Click " + lastX + " " + lastY);
     }
+
     public void mouseDragged(MouseEvent e){}
+
     public void mouseEntered(MouseEvent e){}
+
     public void mouseReleased(MouseEvent e){}
+
     public void mousePressed(MouseEvent e){}
-    
+
     public static void startListening()
     {
         lastX = 0;
         lastY = 0;
     }
-    
+
     public static int getLastX()
     {
         return lastX;
     }
-    
+
     public static int getLastY()
     {
         return lastY;
