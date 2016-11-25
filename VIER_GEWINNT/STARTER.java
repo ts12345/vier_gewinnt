341d8abb5eb40f4d6016516528f7507ac416a5ec
SFr682k <SFr682k@users.noreply.github.com>
2016-11-25 13:35:22 +0100

    Vorbereitung Issue #52

--- VIER_GEWINNT/STARTER.java
+++ VIER_GEWINNT/STARTER.java
@@ -1,14 +1,73 @@
+import java.util.Scanner;
 
 /**
- * Die Klasse STARTER soll einen Startbildschirm erstellen
+ * Objekte der Klasse STARTER starten ein neues Spiel
+ * Eine grafische Oberfläche ist geplant
  * 
- * @author SFr
+ * @author SFr682k
  */
-public class STARTER {
-    /**
-     * Konstruktor für Objekte der Klasse STARTER
-     */
+public class STARTER {   
+    public static void main() {
+        new STARTER();
+    }
+    
     public STARTER() {
+        // Deklariert Variablen für Übergabe an OBERCONTROLLER
+        int size, sp1, sp2;
         
+        // erstellt den Scanner
+        Scanner scan = new Scanner(System.in);
+        
+        // Frägt die Sollspielfeldgröße ab und speichert sie in der sizeinput-Variable ab
+        System.out.println("Bitte geben Sie die Spielfeldgröße ein. Mögliche Werte: 1 - 5 [3]");
+        String sizeinput = scan.nextLine();
+        
+        // weist der Eingabe den entsprechenden size-Wert zu
+        switch(sizeinput) {
+            case "1":
+                size = 50;
+                break;
+                
+            case "2":
+                size = 75;
+                break;
+                
+            case "4":
+                size = 125;
+                break;
+                
+            case "5":
+                size = 150;
+                break;
+            
+            default:
+                size = 100;
+                break;
+        }
+        
+        // Frägt die Typen von Spieler 1 und Spieler 2 ab
+        System.out.println("Bitte geben Sie die Art von Spieler 1 ein (1 - Humanspieler, 2 - Computerspieler, 3/4 - Netzwerkspieler)");
+        String player1 = scan.nextLine();
+        System.out.println("Und jetzt fuer Spieler 2:");
+        String player2 = scan.nextLine();
+        
+        // Bereinigt die Werteeingabe von oben:
+        switch(player1) {
+            case "1": sp1 = 1; break;
+            case "2": sp1 = 2; break;
+            case "3": sp1 = 3; break;
+            case "4": sp1 = 4; break;
+            default: "1": sp1 = 1; break;
+        }
+        
+        switch(player2) {
+            case "1": sp2 = 1; break;
+            case "2": sp2 = 2; break;
+            case "3": sp2 = 3; break;
+            case "4": sp2 = 4; break;
+        }
+        
+        // legt einen neuen OBERCONTROLLER an
+        OBERCONTROLLER hugoDerBoss = new OBERCONTROLLER(size, sp1, sp2);
     }
 }
