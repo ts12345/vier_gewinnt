47215150d6e089ba9a7e3c47ae22e3250582abcd
SFr682k <SFr682k@users.noreply.github.com>
2016-11-25 13:38:05 +0100

    OBERCONTROLLER wurde für Issue #52 geschult

--- VIER_GEWINNT/OBERCONTROLLER.java
+++ VIER_GEWINNT/OBERCONTROLLER.java
@@ -10,45 +10,9 @@ public class OBERCONTROLLER {
 
     // Konstruktor für Objekte der Klasse OBERCONTROLLER
     // Übergabewerte für Feldgröße und Typ der Spieler
-    public OBERCONTROLLER(int sizeVonStart, int sp1, int sp2) {
-        // Legt Variablen zur variablen Festlegung der Spielfeldgröße fest
-        int size;
-        
+    public OBERCONTROLLER(int size, int sp1, int sp2) {
         // erstellt den Scanner
         Scanner scan = new Scanner(System.in);
-        
-        // Frägt im Starter die Sollspielfeldgröße ab und speichert den Wert in der sizeinput-Variable
-        System.out.println("Bitte geben Sie die Spielfeldgröße ein. Mögliche Werte: 1 - 5 [3]");
-        String sizeinput = scan.nextLine();
-        
-        // weist der Eingabe den entsprechenden size-Wert zu
-        switch(sizeinput) {
-            case "1":
-                size = 60;
-                break;
-                
-            case "2":
-                size = 80;
-                break;
-                
-            case "4":
-                size = 120;
-                break;
-                
-            case "5":
-                size = 140;
-                break;
-            
-            default:
-                size = 100;
-                break;
-        }
-        
-        // Legt die Typen der Spieler 1 und 2 fest
-        System.out.println("Bitte geben Sie die Art von Spieler 1 ein (1 - Humanspieler, 2 - Computerspieler, 3 - Netzwerkspieler)");
-        String player = scan.nextLine();
-        System.out.println("Und jetzt fuer Spieler 2:");
-        String player2 = scan.nextLine();
         
         // deklariert Referenzattribute für beide Spieler
         SPIELER s1;
@@ -58,23 +22,23 @@ public class OBERCONTROLLER {
         CONTROLLER commandante = new CONTROLLER(size);
        
         // erstellt Referenz auf die beiden Spieler
-        switch(player){
-            case "2":
+        switch(sp1){
+            case 2:
             s1 = new COMPUTERSPIELER_TS(commandante.spielfeld);
             s2 = new HUMANSPIELER(size);
             break;
-            case "3":
+            case 3:
             s1 = new NETZWERKSPIELER("localhost", 2001);
             s2 = new HUMANSPIELER(size);
             break;
-            case "4":
+            case 4:
             s2 = new NETZWERKSPIELER("localhost", 2002);
             s1 = new HUMANSPIELER(size);
             break;
             default:
             s1 = new HUMANSPIELER(size);
-            switch(player2){
-                case "2":
+            switch(sp2){
+                case 2:
                 s2 = new COMPUTERSPIELER_TS(commandante.spielfeld);
                 break;
                 default:
