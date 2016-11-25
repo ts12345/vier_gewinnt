30cdda28585b6a7a378ced08650eb712d3639058
allgemein <allgemein>
2016-11-25 13:01:56 +0100

    Leerer Starter beigefügt

---
+++ VIER_GEWINNT/STARTER.java
@@ -0,0 +1,14 @@
+
+/**
+ * Die Klasse STARTER soll einen Startbildschirm erstellen
+ * 
+ * @author SFr
+ */
+public class STARTER {
+    /**
+     * Konstruktor für Objekte der Klasse STARTER
+     */
+    public STARTER() {
+        
+    }
+}
