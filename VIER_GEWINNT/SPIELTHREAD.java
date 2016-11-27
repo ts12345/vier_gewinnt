951c24eb97301163ef0f6ff0482bea473cca93b3
SFr682k <SFr682k@users.noreply.github.com>
2016-11-27 11:41:53 +0100

    Weitere Eingaben in den Starter ausgelagert (#52)

--- VIER_GEWINNT/SPIELTHREAD.java
+++ VIER_GEWINNT/SPIELTHREAD.java
@@ -3,17 +3,21 @@ public class SPIELTHREAD implements Runnable
     private int size; 
     private int sp1;
     private int sp2;
+    String serverIP;
+    int hauptport;
 
-    public SPIELTHREAD(int size, int sp1, int sp2) {
+    public SPIELTHREAD(int size, int sp1, int sp2, String serverIP, int hauptport) {
         {   
             this.size = size;
             this.sp1 = sp1;
             this.sp2 = sp2;
+            this.serverIP = serverIP;
+            this.hauptport = hauptport;
         }
     }
 
     public void run() {
-        OBERCONTROLLER controll = new OBERCONTROLLER(size,sp1,sp2);
+        OBERCONTROLLER controll = new OBERCONTROLLER(size, sp1, sp2, serverIP, hauptport);
     }
 
 }
