fa669e8bb7be2c5db7f234d5b879b50c674cd267
SFr682k <SFr682k@users.noreply.github.com>
2016-11-27 14:31:44 +0100

    SPIELTHREAD angepasst

--- VIER_GEWINNT/SPIELTHREAD.java
+++ VIER_GEWINNT/SPIELTHREAD.java
@@ -1,23 +1,19 @@
-public class SPIELTHREAD implements Runnable
-{
+public class SPIELTHREAD implements Runnable {
     private int size; 
-    private int sp1;
-    private int sp2;
+    private int spieler;
     String serverIP;
     int hauptport;
 
-    public SPIELTHREAD(int size, int sp1, int sp2, String serverIP, int hauptport) {
+    public SPIELTHREAD(int size, int spieler, String serverIP, int hauptport) {
         {   
             this.size = size;
-            this.sp1 = sp1;
-            this.sp2 = sp2;
+            this.spieler = spieler;
             this.serverIP = serverIP;
             this.hauptport = hauptport;
         }
     }
 
     public void run() {
-        OBERCONTROLLER controll = new OBERCONTROLLER(size, sp1, sp2, serverIP, hauptport);
+        OBERCONTROLLER controll = new OBERCONTROLLER(size, spieler, serverIP, hauptport);
     }
-
 }
