a543bb44cd7e766a6b991197e209cb719bb16312
SFr682k <SFr682k@users.noreply.github.com>
2016-11-27 14:31:36 +0100

    OBERCONTROLLER lost aus!
    OBERCONTROLLER entscheidet jetzt mit WILLKÜR, wer beim Spiel Mensch gegen Maschine beginnen darf!

--- VIER_GEWINNT/OBERCONTROLLER.java
+++ VIER_GEWINNT/OBERCONTROLLER.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Random;
 
 /**
  * Objekte der Klasse OBERCONTROLLER verwalten den Ablauf eines Netzwerkspiels
@@ -6,7 +7,7 @@ import java.util.Scanner;
 public class OBERCONTROLLER {
     // Konstruktor für Objekte der Klasse OBERCONTROLLER
     // Übergabewerte für Feldgröße und Typ der Spieler
-    public OBERCONTROLLER(int size, int sp1, int sp2, String serverIP, int hauptport) {
+    public OBERCONTROLLER(int size, int spieler, String serverIP, int hauptport) {
         // erstellt den Scanner
         Scanner scan = new Scanner(System.in);
         
@@ -14,34 +15,39 @@ public class OBERCONTROLLER {
         SPIELER s1;
         SPIELER s2;
         
+        // erstellt auf ein Objekt, das Zufallszahlen erzeugt, Referenz
+        Random random = new Random();
+        
         // deklariert und erstellt Referenz auf einen Controller
         CONTROLLER commandante = new CONTROLLER(size);
        
         // erstellt Referenz auf die beiden Spieler
-        switch(sp1){
+        switch(spieler){
             case 2:
-            s1 = new COMPUTERSPIELER_TS(commandante.spielfeld);
-            s2 = new HUMANSPIELER(size);
-            break;
+                // Startspieler wird zufällig ermittelt
+                int startspieler = random.nextInt(2);
+                
+                if(startspieler == 0) {
+                    s1 = new HUMANSPIELER(size);
+                    s2 = new COMPUTERSPIELER_TS(commandante.spielfeld);
+                } else {
+                    s1 = new COMPUTERSPIELER_TS(commandante.spielfeld);
+                    s2 = new HUMANSPIELER(size);
+                }
+                break;
                 
             case 3:
-            s1 = new NETZWERKSPIELER(serverIP, 2001);
-            s2 = new HUMANSPIELER(size);
-            break;
-
-            case 4:
-            s2 = new NETZWERKSPIELER(serverIP, 2002);
-            s1 = new HUMANSPIELER(size);
-            break;
-                
-            default:
-            s1 = new HUMANSPIELER(size);
-            switch(sp2){
-                case 2:
-                s2 = new COMPUTERSPIELER_TS(commandante.spielfeld);
+                s1 = new NETZWERKSPIELER(serverIP, 2001);
+                s2 = new HUMANSPIELER(size);
                 break;
 
-                default:
+            case 4:
+                s2 = new NETZWERKSPIELER(serverIP, 2002);
+                s1 = new HUMANSPIELER(size);
+                break;
+                
+            default:
+                s1 = new HUMANSPIELER(size);
                 s2 = new HUMANSPIELER(size);
             }
         }
