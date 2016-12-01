e6a90b53b483b6a1ef344cf60db8c197f0a0256f
SFr682k <sfr682k@you-spam.com>
2016-12-01 17:53:48 +0100

    Klasse INDEX vollständig - Issues #53 und #54
    Hallo @hobbyduck, die Positionen mit den indizerten Wörtern können nun
    belegt werden.
    Dazu müssen nur die Variable size (Z. 19) und die einzelnen Positionen
    des Index (Z. 32ff) geändert werden (vgl. Kommentare)
    
    Und wie @ts12345 bereits erwähnt hat: bitte nur wirklich harmlose
    Wörter!

--- VIER_GEWINNT/INDEX.java
+++ VIER_GEWINNT/INDEX.java
@@ -1,4 +1,3 @@
-
 /**
  * Objekte der Klasse INDEX teilen dem ZENSOR mit, welche Phrasen zensiert werden
  * Die INDEX-Klasse befindet sich in Bearbeitung
@@ -7,32 +6,55 @@
  * @version 2016-12-01
  */
 public class INDEX {
-    // deklariert Variablen für die Größe des Arrays
-    private int width = 15;  // 15 Felder in x-Richtung, nicht ändern!
-    private int length;      //  y Felder in y-Richtung
-    
-    // deklariert Variablen für die Anzahl der Wörter im Array
+    // deklariert Variable für die Größe des Arrays
     private int size;
     
     // deklariert ein Array für den Index
-    String[][] index;
+    String[] index;
 
     // Konstruktor
     public INDEX() {
         // HIER manuell die Anzahl der indizierten Wörter einfügen
-        size = 1;
-        
-        // Hier wird demnächst die benötigte Anzahl der Felder in y-Richtung berechnet
-        length = 1;
-        
+        // WICHTIG:   Letztes Wort = Position in Array + 1   !!!!
+        size = 3;
+                
         // Initialisiert das Array für die indizierten Wörter
-        index = new String[width][length];
+        index = new String[size];
         
-        // Weiterer Quellcode folgt
+        // Lädt die auf dem Index vorhandenen Wörter
+        loadIndex();
     }
 
     // Methode, die zu zensierende Wörter auf den Index setzt
-    public void getIndex() {
-        // @hobbyduck, eine Vorbereitung für Ihre Liste kommt noch
+    public void loadIndex() {
+        // Schreibt die Liste der zu zensierenden Wörter in den Index
+        // Bitte bei "0" beginnen!!
+        index[0] = "Wort 1";
+        index[1] = "Wort 2";
+        index[2] = "Wort 3";
+    }
+    
+    // Methode, die die Größe des Index zurückgibt
+    public int getIndexSize() {
+        return size;
+    }
+    
+    // Methode, die das Wort aus Stelle x des Index zurückgibt
+    public String getIndexValue(int position) {
+        // Deklariert einen Zwischenspeicher
+        String wort;
+        
+        // Prüft die Eingabe; gibt bei fehlerhaften Werten das erste/letzte Wort zurück
+        if (position < 0) {
+            position = 0;
+        } else if (position >= size) {
+            position = size - 1;
+        }
+        
+        // Lädt das Wort an Position x in den Zwischenspeicher
+        wort = index[position];
+        
+        // Gibt den Inhalt des Zwischenspeichers zurück
+        return wort;
     }
 }
