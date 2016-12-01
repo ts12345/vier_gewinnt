053d6ff6fd5d5c73a51d675fdf6bc311cfba35c6
SFr682k <sfr682k@you-spam.com>
2016-12-01 19:10:49 +0100

    ZENSOR aktualisiert (Issue #53)

--- VIER_GEWINNT/ZENSOR.java
+++ VIER_GEWINNT/ZENSOR.java
@@ -1,33 +1,65 @@
-
 /**
- * Write a description of class ZENSOR here.
+ * Objekte der Klasse ZENSOR zensieren unangebrachte Wörter in Zeichenketten
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author SFr682k
+ * @version 2016-12-01
  */
-public class ZENSOR
-{
-    // instance variables - replace the example below with your own
-    private int x;
+public class ZENSOR {
+    // deklariert ein Referenzattribut für den INDEX
+    INDEX schindlersListe;
 
-    /**
-     * Constructor for objects of class ZENSOR
-     */
-    public ZENSOR()
-    {
-        // initialise instance variables
-        x = 0;
+    // Konstruktor
+    public ZENSOR() {
+        // Erstellt Referenz auf den INDEX
+        schindlersListe = new INDEX();
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    // Methode, die eine Eingabezeichenkette bereinigt zurückgibt
+    public String zensiereText(String input) {
+        // Deklariert eine Variable für den Rückgabetext:
+        String output = "";
+        
+        // Deklariert ein Array, das sagt, welche Zeichen ersetzt werden müssen
+        boolean[] stelleZensieren;
+        
+        // Ermittelt die Länge des eingegeben Textes
+        // NOTIZ: Zählung beginnt bei 1
+        int inputLength = input.length();
+        
+        // Initialisiert das Zensurarray
+        stelleZensieren = new boolean[inputLength];
+        
+        // Standardmäßig werden alle Zeichen nicht zensiert
+        // Entsprechende Vorbelegung des Zensurarrays
+        for(int i = 0; i < inputLength; i++) {
+            stelleZensieren[i] = false;
+        }
+        
+        // Die folgende Zensurpozedur muss noch (üb-)erarbeitet werden:
+            // Durchforstet mit Hilfe von gezählten Wiederholungen die Zeichenkette
+            for(int i = 0; i < inputLength; i++) {
+                for (int j = inputLength; j > i; j--) {
+                    // hier muss ich noch Code hinzufügen ...
+                }
+            }
+        
+            // deklariert das Zensurzeichen:
+            String zensurzeichen = "*";
+            
+            // geht den Eingabetext Zeichen für Zeichen durch
+            // Dabei werden zu zensierende Zeichen durch das Zensurzeichen ersetzt
+            for(int i = 0; i < inputLength; i++) {
+                if(stelleZensieren[i] == true) {
+                    output = output + zensurzeichen;
+                } else {
+                    output = output + input.charAt(i);
+                }
+            }
+            
+        // Da obige Zensurprozedur noch nicht funktioniert, Rückgabe des Eingabewerts
+        output = input;
+        
+        // Gibt die bereinigte Version der Eingabe zurück
+        return output;
     }
 }
