546aa5ebc32b1764fb144a91ed737a2b3ef6b438
SFr682k <sfr682k@you-spam.com>
2016-12-07 14:56:04 +0100

    Der Index ist umgezogen

--- VIER_GEWINNT/INDEX.java
+++ VIER_GEWINNT/INDEX.java
@@ -4,17 +4,13 @@ import java.io.*;
 
 /**
  * Objekte der Klasse INDEX teilen dem ZENSOR mit, welche Phrasen zensiert werden
- * Die INDEX-Klasse befindet sich in Bearbeitung
  * 
  * @author SFr682k and the one and only hobbyduck
- * @version 2016-12-01
+ * @version 2016-12-07
  */
-public class INDEX{
+public class INDEX {
     // deklariert Variable für die Größe des Arrays
     private List<String> index;
-
-    // deklariert ein Array für den Index
-    //private String[] index;
 
     // Konstruktor
     public INDEX() {
@@ -22,25 +18,25 @@ public class INDEX{
         loadIndex();
     }
 
-    List<String> getList(){
+    List<String> getList() {
         return new LinkedList(index);
     }
 
     // Methode, die zu zensierende Wörter auf den Index setzt
-    public void loadIndex(){
-        try{
-            FileReader fr = new FileReader("Zensor/Zensor.txt");
+    public void loadIndex() {
+        try {
+            FileReader fr = new FileReader("INDEX/INDEX.txt");
             BufferedReader br = new BufferedReader(fr);
             String zeile;
-            do{
+            
+            do {
                 zeile = br.readLine();
                 index.add(0,zeile);
-            }
-            while(!zeile.startsWith("#END#"));
+            } while(!zeile.startsWith("#END#"));
+            
             index.remove(0);
-            }
-            catch (Exception e){
-                System.out.println(e.toString());
+        } catch (Exception e){
+            System.out.println(e.toString());
         }
     }
 }
