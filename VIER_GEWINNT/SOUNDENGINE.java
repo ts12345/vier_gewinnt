cded51b2abd98780aa38ec77608be59fc0577da0
SFr682k <sfr682k@you-spam.com>
2016-12-09 23:43:37 +0100

    SOUNDENGINE erweitert

--- VIER_GEWINNT/SOUNDENGINE.java
+++ VIER_GEWINNT/SOUNDENGINE.java
@@ -1,28 +1,45 @@
 import java.io.File;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+
 /**
- * Die Klasse SOUNDENGINE spielt die Sounds Click und Fanfare ab.
+ * Die Klasse SOUNDENGINE spielt Sounds ab.
  * @author (kinder)
  * @version (08.06.16)
  * @since (21.04.16)
  * 
  */
-public class SOUNDENGINE
-{
-    File click;
-    File fanfare;
-    File drawer;
+public class SOUNDENGINE {
+    // Töne für während des Spiels
+    File click, illegal;
+    
+    // Töne für Spielende
+    File fanfare, drawer, lost;
+    
+    // Hauptthema
+    File main;
+    
     public SOUNDENGINE() {
-        File click = new File("sounds/click.wav"); 
+        File click = new File("sounds/click.wav");
+        File illegal = new File("sounds/illegal.wav");
+        
         File fanfare = new File("sounds/fanfare.wav");
         File drawer = new File("sounds/drawer.wav");
-        this.click = click;
-        this.fanfare = fanfare;
-        this.drawer = drawer;
+        File lost = new File("sounds/lost.wav");
+        
+        File main = new File("sounds/main.wav");
+        
+        this.click = click;         // Click-Sound für legale Züge
+        this.illegal = illegal;     // Fehler-Sound für illegale Züge
+        
+        this.fanfare = fanfare;     // Fanfaren-Sound für Sieg
+        this.drawer = drawer;       // Sound für unentschieden
+        this.lost = lost;           // Sound für verloren
+        
+        this.main = main;           // Haupthema
     }
 
-    public static void PlaySound(File Sound){
+    public static void playSound(File Sound) {
         try {
             Clip clip = AudioSystem.getClip();
             clip.open(AudioSystem.getAudioInputStream(Sound));
@@ -32,15 +49,12 @@ public class SOUNDENGINE
         }
     }
 
-    public void playClick() {
-        PlaySound(click);
-    }
+    public void playClick()     { playSound(click); }
+    public void playIllegal()   { playSound(illegal); }
     
-    public void playFanfare() {
-        PlaySound(fanfare);
-    }
+    public void playFanfare()   { playSound(fanfare); }
+    public void playDrawer()    { playSound(drawer); }
+    public void playLost()      { playSound(lost); }
     
-    public void playDrawer() {
-        PlaySound(drawer);
-    }
+    public void playMain()      { playSound(main); }
 }
